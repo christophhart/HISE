3aeaa973a80622018d6c4c844c315c49c1c48485
Christoph Hart <christoph-hart@gmx.de>
2016-03-29 14:40:21 +0200

    - @internal: moved api_generator to tools subfolder - @internal: added build script for windows

---
+++ tools/api generator/batchCreate.bat
@@ -0,0 +1,69 @@
+echo off
+
+del "xml\selection" /Q
+rd "xml\selection"
+
+doxygen "xml.doxyfile"
+
+md "xml\selection"
+
+xcopy "xml\class_scripting_api_1_1_console.xml" "xml\selection"
+xcopy "xml\class_scripting_api_1_1_content.xml" "xml\selection"
+xcopy "xml\class_scripting_api_1_1_engine.xml" "xml\selection"
+xcopy "xml\class_scripting_api_1_1_message.xml" "xml\selection"
+xcopy "xml\class_scripting_api_1_1_synth.xml" "xml\selection"
+xcopy "xml\class_scripting_api_1_1_sampler.xml" "xml\selection"
+xcopy "xml\class_scripting_objects_1_1_scripting_modulator.xml" "xml\selection"
+xcopy "xml\class_scripting_objects_1_1_midi_list.xml" "xml\selection"
+xcopy "xml\class_scripting_objects_1_1_scripting_audio_sample_processor.xml" "xml\selection"
+xcopy "xml\class_scripting_objects_1_1_scripting_effect.xml" "xml\selection"
+xcopy "xml\class_scripting_objects_1_1_scripting_midi_processor.xml" "xml\selection"
+xcopy "xml\class_scripting_objects_1_1_scripting_preset_storage.xml" "xml\selection"
+
+
+xcopy "xml\struct_scripting_api_1_1_content_1_1_script_button.xml" "xml\selection"
+xcopy "xml\struct_scripting_api_1_1_content_1_1_script_combo_box.xml" "xml\selection"
+xcopy "xml\struct_scripting_api_1_1_content_1_1_script_slider.xml" "xml\selection"
+xcopy "xml\struct_scripting_api_1_1_content_1_1_script_label.xml" "xml\selection"
+xcopy "xml\struct_scripting_api_1_1_content_1_1_modulator_meter.xml" "xml\selection"
+xcopy "xml\struct_scripting_api_1_1_content_1_1_script_table.xml" "xml\selection"
+xcopy "xml\struct_scripting_api_1_1_content_1_1_scripted_plotter.xml" "xml\selection"
+xcopy "xml\struct_scripting_api_1_1_content_1_1_script_image.xml" "xml\selection"
+xcopy "xml\struct_scripting_api_1_1_content_1_1_script_slider_pack.xml" "xml\selection"
+
+
+ren "xml\selection\class_scripting_api_1_1_console.xml" "Console.xml"
+ren "xml\selection\class_scripting_api_1_1_content.xml" "Content.xml"
+ren "xml\selection\class_scripting_api_1_1_engine.xml" "Engine.xml"
+ren "xml\selection\class_scripting_api_1_1_message.xml" "Message.xml"
+ren "xml\selection\class_scripting_api_1_1_synth.xml" "Synth.xml"
+ren "xml\selection\class_scripting_api_1_1_sampler.xml" "Sampler.xml"
+ren "xml\selection\class_scripting_objects_1_1_scripting_modulator.xml" "Modulator.xml"
+ren "xml\selection\class_scripting_objects_1_1_midi_list.xml" "MidiList.xml"
+ren "xml\selection\class_scripting_objects_1_1_scripting_effect.xml" "Effect.xml"
+ren "xml\selection\class_scripting_objects_1_1_scripting_midi_processor.xml" "MidiProcessor.xml"
+ren "xml\selection\class_scripting_objects_1_1_scripting_audio_sample_processor.xml" "AudioSampleProcessor.xml"
+ren "xml\selection\class_scripting_objects_1_1_scripting_synth.xml" "ChildSynth.xml"
+ren "xml\selection\class_scripting_objects_1_1_scripting_preset_storage.xml" "PresetStorage.xml"
+
+ren "xml\selection\struct_scripting_api_1_1_content_1_1_script_button.xml" "ScriptButton.xml"
+ren "xml\selection\struct_scripting_api_1_1_content_1_1_script_combo_box.xml" "ScriptComboBox.xml"
+ren "xml\selection\struct_scripting_api_1_1_content_1_1_script_slider.xml" "ScriptSlider.xml"
+ren "xml\selection\struct_scripting_api_1_1_content_1_1_script_label.xml" "ScriptLabel.xml"
+ren "xml\selection\struct_scripting_api_1_1_content_1_1_modulator_meter.xml" "ModulatorMeter.xml"
+ren "xml\selection\struct_scripting_api_1_1_content_1_1_script_table.xml" "ScriptTable.xml"
+ren "xml\selection\struct_scripting_api_1_1_content_1_1_script_image.xml" "ScriptImage.xml"
+ren "xml\selection\struct_scripting_api_1_1_content_1_1_scripted_plotter.xml" "ScriptedPlotter.xml"
+ren "xml\selection\struct_scripting_api_1_1_content_1_1_script_slider_pack.xml" "ScriptSliderPack.xml"
+
+
+ApiExtractor.exe xml\selection xml\selection
+
+del xml\selection\*.xml /Q
+
+BinaryBuilder.exe xml\selection "..\hi_core\hi_scripting\scripting\api" XmlApi
+
+del "xml\selection" /Q
+REM #rd "xml\selection"
+
+
---
+++ tools/auto_build/ConfigWindows.bat
@@ -0,0 +1,12 @@
+
+REM This is the local folder where the DMG images are created.
+SET nightly_build_folder="D:\Development\Installer\Windows\nightly_builds"
+
+REM This is the project folder for the Standalone app
+SET standalone_project="D:\Development\Projekte\HISE Standalone\Builds\VisualStudio2013\HISE Standalone.sln"
+
+REM This is the project folder of the plugin project
+SET plugin_project="D:\Development\Projekte\HISE Plugin\Builds\VisualStudio2013\HISE.sln"
+
+REM This is the path to the ISS Installer compiler
+SET installer_command="C:\Program Files (x86)\Inno Setup 5\ISCC.exe"
