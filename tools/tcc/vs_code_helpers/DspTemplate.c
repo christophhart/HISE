33e74c490fdd4653d1b26b96b44c2c2dc8881901
Christoph Hart <christoph-hart@gmx.de>
2016-08-16 14:26:58 +0200

    - added TCC files

---
+++ tools/tcc/vs_code_helpers/DspTemplate.c
@@ -0,0 +1,65 @@
+/** Template C File for DSP modules. 
+*
+*   Use this file to implement your DSP routines and load it with the embedded TCC compiler
+*
+*	If you put the code in the correct sections, it can be automatically converted to a CPP class.
+*/
+
+
+#include <tcclib.h>
+#include <TccLibrary.h>
+
+// Will be replaced by the correct CPP keyword when converting
+#define CPP_CONST
+#define CPP_OVERRIDE
+
+// [INCLUDE SECTION] ****************************************************************************************
+
+// [/INCLUDE SECTION]
+
+// [PRIVATE MEMBER SECTION] *********************************************************************************
+
+// [/PRIVATE MEMBER SECTION]
+
+
+// [CALLBACK SECTION] ***************************************************************************************
+void initialise()
+{
+
+}
+
+void release() {}
+
+int getNumParameters() CPP_CONST CPP_OVERRIDE { return 1; }
+
+float getParameter(int index) CPP_CONST CPP_OVERRIDE { return 1.0f; }
+
+void setParameter(int index, float newValue) CPP_OVERRIDE {}
+
+void prepareToPlay(double sampleRate, int samplesPerBlock) CPP_OVERRIDE {}
+
+void processBlock(float** data, int numChannels, int numSamples) CPP_OVERRIDE 
+{
+    
+}
+// [/CALLBACK SECTION]
+
+// [MAIN SECTION] *******************************************************************************************
+int main()
+{
+	// A dummy test routine...
+    initialise();
+
+    float l[512];
+    float r[512];
+
+    float *channels[2] = {l, r};
+
+    prepareToPlay(44100, 512);
+    processBlock(channels, 2, 512);
+
+    printString("Rendered successful");
+
+    return 0;
+}
+// [/MAIN SECTION]
