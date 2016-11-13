77e91c0ee42997ca7b1baccc03355aa8277fd00b
Christoph Hart <christoph-hart@gmx.de>
2016-11-13 22:52:19 +0100

    Build version update: 646

--- extras/demo_project/Scripts/externalScript.js
+++ extras/demo_project/Scripts/externalScript.js
@@ -1,13 +1,4 @@
-/** External script
-*
-*	You can also write external scripts and include them into your scripts via `include("filename.js");`.
-*
-*	They will be parsed when the script is loaded. This is useful for boring data definitions or common API enhancements.
-*/
-
-Console.print("This external script is also compiled");
-
-external = Content.addLabel("external", 0, 0);
-
-// Don't store this control in a preset
-external.set("saveInPreset", false);
+inline function getX()
+{
+	return 2;
+}
