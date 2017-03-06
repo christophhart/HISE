7156a3fd34eec4401e89c79a8140bb01e66ad3fd
Christoph Hart <christoph-hart@gmx.de>
2017-03-06 11:05:14 +0100

    - added plugin client code to standalone

--- projects/standalone/JuceLibraryCode/AppConfig.h
+++ projects/standalone/JuceLibraryCode/AppConfig.h
@@ -32,6 +32,7 @@
 #define JUCE_MODULE_AVAILABLE_juce_audio_basics               1
 #define JUCE_MODULE_AVAILABLE_juce_audio_devices              1
 #define JUCE_MODULE_AVAILABLE_juce_audio_formats              1
+#define JUCE_MODULE_AVAILABLE_juce_audio_plugin_client        1
 #define JUCE_MODULE_AVAILABLE_juce_audio_processors           1
 #define JUCE_MODULE_AVAILABLE_juce_audio_utils                1
 #define JUCE_MODULE_AVAILABLE_juce_core                       1
@@ -193,6 +194,17 @@
 
 #ifndef    JUCE_USE_WINDOWS_MEDIA_FORMAT
  //#define JUCE_USE_WINDOWS_MEDIA_FORMAT
+#endif
+
+//==============================================================================
+// juce_audio_plugin_client flags:
+
+#ifndef    JUCE_FORCE_USE_LEGACY_PARAM_IDS
+ //#define JUCE_FORCE_USE_LEGACY_PARAM_IDS
+#endif
+
+#ifndef    JUCE_USE_STUDIO_ONE_COMPATIBLE_PARAMETERS
+ //#define JUCE_USE_STUDIO_ONE_COMPATIBLE_PARAMETERS
 #endif
 
 //==============================================================================
--- projects/standalone/JuceLibraryCode/JuceHeader.h
+++ projects/standalone/JuceLibraryCode/JuceHeader.h
@@ -23,6 +23,7 @@
 #include <juce_audio_basics/juce_audio_basics.h>
 #include <juce_audio_devices/juce_audio_devices.h>
 #include <juce_audio_formats/juce_audio_formats.h>
+#include <juce_audio_plugin_client/juce_audio_plugin_client.h>
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_audio_utils/juce_audio_utils.h>
 #include <juce_core/juce_core.h>
