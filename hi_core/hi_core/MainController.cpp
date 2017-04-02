f1b471bd4587223f4f0be62ca9bc38c9863d64c7
Christoph Hart <christoph-hart@gmx.de>
2017-04-02 13:58:30 +0200

    - fixed MIDI event unit tests

--- hi_core/hi_core/MainController.cpp
+++ hi_core/hi_core/MainController.cpp
@@ -243,15 +243,15 @@ void MainController::stopCpuBenchmark()
 {
 	const float thisUsage = 100.0f * (float)((Time::highResolutionTicksToSeconds(Time::getHighResolutionTicks()) - temp_usage) * sampleRate / bufferSize.get());
 	
-	const float lastUsage = usagePercent.get();
+	const float lastUsage = usagePercent.load();
 	
 	if (thisUsage > lastUsage)
 	{
-		usagePercent.set(thisUsage);
+		usagePercent.store(thisUsage);
 	}
 	else
 	{
-		usagePercent.set(lastUsage*0.99f);
+		usagePercent.store(lastUsage*0.99f);
 	}
 }
 
