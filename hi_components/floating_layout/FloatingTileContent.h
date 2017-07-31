3d14c72b8125597e38a2692a8e7147642d287973
Christoph Hart <christoph-hart@gmx.de>
2017-07-31 12:10:47 +0200

    - fixed font size for JSON editor so that it will be visible on macOS sierra (yay)...

--- hi_components/floating_layout/FloatingTileContent.h
+++ hi_components/floating_layout/FloatingTileContent.h
@@ -132,7 +132,7 @@ public:
 		editor->setColour(CaretComponent::ColourIds::caretColourId, Colour(0xFFDDDDDD));
 		editor->setColour(ScrollBar::ColourIds::thumbColourId, Colour(0x3dffffff));
 
-		editor->setFont(GLOBAL_MONOSPACE_FONT().withHeight(16.0f));
+		editor->setFont(GLOBAL_MONOSPACE_FONT().withHeight(17.0f));
 
 		constrainer.setMinimumWidth(200);
 		constrainer.setMinimumHeight(300);
