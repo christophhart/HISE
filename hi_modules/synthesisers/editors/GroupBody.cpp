132878f89e74e34d7fbe473585797c3b15947b60
Christoph Hart <christoph-hart@gmx.de>
2017-11-20 10:07:12 +0100

    - increased SynthesiserGroup spread to 200%

--- hi_modules/synthesisers/editors/GroupBody.cpp
+++ hi_modules/synthesisers/editors/GroupBody.cpp
@@ -171,6 +171,7 @@ GroupBody::GroupBody (ProcessorEditor *p)
 
 	spreadSlider->setup(getProcessor(), ModulatorSynthGroup::SpecialParameters::UnisonoSpread, "Spread");
 	spreadSlider->setMode(HiSlider::Mode::NormalizedPercentage);
+	spreadSlider->setRange(0.0, 2.0, 0.01);
 
 	spreadSlider->setIsUsingModulatedRing(true);
 	detuneSlider->setIsUsingModulatedRing(true);
