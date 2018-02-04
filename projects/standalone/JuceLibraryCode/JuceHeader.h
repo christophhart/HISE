832259669f941884701bf8fc59ee19c2ce379629
Christoph Hart <christoph-hart@gmx.de>
2018-02-04 12:46:26 +0100

    - bumped Projucer project version

--- projects/standalone/JuceLibraryCode/JuceHeader.h
+++ projects/standalone/JuceLibraryCode/JuceHeader.h
@@ -51,7 +51,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "HISE Standalone";
-    const char* const  versionString  = "1.5.0";
-    const int          versionNumber  = 0x10500;
+    const char* const  versionString  = "1.5.1";
+    const int          versionNumber  = 0x10501;
 }
 #endif
