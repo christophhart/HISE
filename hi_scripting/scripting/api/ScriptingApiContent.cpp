acf0f692871adee29184f1c2dfbda3907e280389
Christoph Hart <christoph-hart@gmx.de>
2018-06-01 20:07:12 +0200

    - fixed assertion

--- hi_scripting/scripting/api/ScriptingApiContent.cpp
+++ hi_scripting/scripting/api/ScriptingApiContent.cpp
@@ -3552,6 +3552,8 @@ void ScriptingApi::Content::ScriptFloatingTile::setScriptObjectPropertyWithChang
 {
 	if (id == getIdFor(ContentType))
 	{
+		MessageManagerLock mm;
+
 		FloatingTile ft(getScriptProcessor()->getMainController_(), nullptr);
 
 		ft.setNewContent(newValue.toString());
