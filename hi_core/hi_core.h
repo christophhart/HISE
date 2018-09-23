1bf8deb037a445833e1606bc258310a778d498dd
Christoph Hart <christoph-hart@gmx.de>
2018-09-24 00:43:38 +0200

    - disabled startup log

--- hi_core/hi_core.h
+++ hi_core/hi_core.h
@@ -226,7 +226,7 @@ Set this to 0 to disable host information like tempo, playing position etc...
 If this is enabled, compiled plugins will write a startup log to the desktop for debugging purposes
 */
 #ifndef ENABLE_STARTUP_LOG
-#define ENABLE_STARTUP_LOG 1
+#define ENABLE_STARTUP_LOG 0
 #endif
 
 
