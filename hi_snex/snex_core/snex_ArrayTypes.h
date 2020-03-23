f0d2acff939ec1a617160b99712258068797a10a
chrisboy2000 <christoph-hart@gmx.de>
2020-03-23 17:28:22 +0100

    - SNEX: added native dsp functions

---
+++ hi_snex/snex_core/snex_ArrayTypes.h
@@ -0,0 +1,745 @@
+/*  ===========================================================================
+*
+*   This file is part of HISE.
+*   Copyright 2016 Christoph Hart
+*
+*   HISE is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option any later version.
+*
+*   HISE is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with HISE.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Commercial licences for using HISE in an closed source project are
+*   available on request. Please visit the project's website to get more
+*   information about commercial licencing:
+*
+*   http://www.hartinstruments.net/hise/
+*
+*   HISE is based on the JUCE library,
+*   which also must be licenced for commercial applications:
+*
+*   http://www.juce.com
+*
+*   ===========================================================================
+*/
+
+#pragma once
+
+#include <cstring>
+#include <malloc.h>
+#include <type_traits>
+#include <initializer_list>
+#include <nmmintrin.h>
+#include <stdint.h>
+
+namespace snex {
+namespace Types {
+
+
+struct DSP
+{
+	template <class WrapType, class T> static float interpolate(T& data, float v)
+	{
+		static_assert(std::is_same<T, typename WrapType::ParentType>(), "parent type mismatch ");
+
+		auto floorValue = (int)v;
+
+		WrapType lower = { (int)v };
+		WrapType upper = { lower.value + 1 };
+
+		int lowerIndex = lower.get(data);
+		int upperIndex = upper.get(data);
+
+		auto ptr = data.begin();
+		auto lowerValue = ptr[lowerIndex];
+		auto upperValue = ptr[upperIndex];
+		auto alpha = v - (float)floorValue;
+		const float invAlpha = 1.0f - alpha;
+		return invAlpha * lowerValue + alpha * upperValue;
+	}
+};
+
+template <class T, int MaxSize> struct span
+{
+	using DataType = T;
+	using Type = span<T, MaxSize>;
+
+	static constexpr int s = MaxSize;
+
+	struct wrapped
+	{
+		using ParentType = Type;
+
+		wrapped& operator=(int v) { value = v; return *this; }
+
+		int get(const Type& obj) const
+		{
+			auto i = value % MaxSize;
+			return i;
+		}
+
+		int value = 0;
+	};
+
+	struct zeroed
+	{
+		using ParentType = Type;
+
+		zeroed& operator=(int v) { value = v; return *this; }
+
+		int get(const Type& obj) const
+		{
+			if (isPositiveAndBelow(value, MaxSize - 1))
+				return value;
+
+			return 0;
+		}
+
+		int value = 0;
+	};
+
+	struct clamped
+	{
+		using ParentType = Type;
+
+		clamped& operator=(int v) { value = v; return *this; }
+
+		int get(const Type& obj) const
+		{
+			return jlimit(0, MaxSize - 1, value);
+		}
+
+		int value = 0;
+	};
+
+	struct unsafe
+	{
+		using ParentType = Type;
+
+		unsafe& operator=(int v) { value = v; return *this; }
+
+		int get(const Type& obj) const
+		{
+			return value;
+		}
+
+		int value = 0;
+	};
+
+	template <class IndexType> IndexType index(int value)
+	{
+		IndexType obj = { value };
+		auto v = obj.get(*this);
+		obj.value = v;
+		return obj;
+	}
+
+	span()
+	{
+		memset(data, 0, sizeof(T)*MaxSize);
+	}
+
+	span(const std::initializer_list<T>& l)
+	{
+		if (l.size() == 1)
+		{
+			for (int i = 0; i < MaxSize; i++)
+			{
+				data[i] = *l.begin();
+			}
+		}
+		else
+			memcpy(data, l.begin(), sizeof(T)*MaxSize);
+
+	}
+
+	static constexpr size_t getSimdSize()
+	{
+		static_assert(isSimdable(), "not SIMDable");
+
+		if (std::is_same<T, float>())
+			return 4;
+		else
+			return 2;
+	}
+
+	Type& operator=(const T& t)
+	{
+		static_assert(isSimdable(), "Can't add non SIMDable types");
+
+		constexpr int numLoop = MaxSize / getSimdSize();
+		int i = 0;
+
+		float t_ = (float)t;
+
+		if (std::is_same<T, float>())
+		{
+			auto dst = (float*)data;
+
+			for (int i = 0; i < numLoop; i++)
+			{
+				auto v = _mm_load_ps1(&t_);
+				_mm_store_ps(dst, v);
+
+				dst += getSimdSize();
+			}
+		}
+
+		return *this;
+	}
+
+	operator DataType()
+	{
+		if (MaxSize == 1)
+			return *begin();
+	}
+
+	Type& operator+=(const T& scalar)
+	{
+		*this + scalar;
+		return *this;
+	}
+
+	Type& operator+(const T& scalar)
+	{
+		static_assert(isSimdable(), "Can't add non SIMDable types");
+
+		constexpr int numLoop = MaxSize / getSimdSize();
+		auto dst = (float*)data;
+		auto sc = (float)scalar;
+
+		auto v = _mm_load_ps1(&scalar);
+
+		for (int i = 0; i < numLoop; i++)
+		{
+			auto r = _mm_add_ps(_mm_load_ps(dst), v);
+			_mm_store_ps(dst, r);
+
+			dst += getSimdSize();
+		}
+
+		return *this;
+	}
+
+	Type& operator=(const Type& other)
+	{
+		static_assert(isSimdable(), "Can't add non SIMDable types");
+
+		constexpr int numLoop = MaxSize / getSimdSize();
+		auto src = (float*)other.data;
+		auto dst = (float*)data;
+
+		for (int i = 0; i < numLoop; i++)
+		{
+			auto v = _mm_load_ps((float*)src);
+			_mm_store_ps(dst, v);
+
+			src += getSimdSize();
+			dst += getSimdSize();
+		}
+
+		return *this;
+	}
+
+	Type& operator+ (const Type& other)
+	{
+		auto dst = (float*)data;
+		auto src = (float*)other.data;
+		constexpr int numLoop = MaxSize / getSimdSize();
+
+		for (int i = 0; i < numLoop; i++)
+		{
+			auto v = _mm_load_ps(dst);
+			auto r = _mm_add_ps(v, _mm_load_ps(src));
+			_mm_store_ps(dst, r);
+
+			dst += getSimdSize();
+			src += getSimdSize();
+		}
+
+		return *this;
+	}
+
+	Type& operator +=(const Type& other)
+	{
+		static_assert(isSimdable(), "Can't add non SIMDable types");
+
+		constexpr int numLoop = MaxSize / getSimdSize();
+		int i = 0;
+
+		auto dst = (float*)data;
+		auto src = (float*)other.data;
+
+		for (int i = 0; i < numLoop; i++)
+		{
+			auto v = _mm_load_ps(dst);
+			auto r = _mm_add_ps(v, _mm_load_ps(src));
+			_mm_store_ps(dst, r);
+
+			dst += getSimdSize();
+			src += getSimdSize();
+		}
+
+		return *this;
+	}
+
+	T accumulate() const
+	{
+		T v = T(0);
+
+		for (int i = 0; i < MaxSize; i++)
+		{
+			v += data[i];
+		}
+
+		return v;
+	}
+
+	static constexpr bool isSimdType()
+	{
+		return (std::is_same<T, float>() && MaxSize == 4) ||
+			(std::is_same<T, double>() && MaxSize == 2);
+	}
+
+	static constexpr bool isSimdable()
+	{
+		return (std::is_same<T, float>() && MaxSize % 4 == 0) ||
+			(std::is_same<T, double>() && MaxSize % 2 == 0);
+	}
+
+	bool isAlignedTo16Byte() const
+	{
+		return isAlignedTo16Byte(*this);
+	}
+
+
+	template <class WrapType> float interpolate(float index)
+	{
+		return DSP::interpolate<WrapType>(*this, index);
+	}
+
+	template <class ObjectType> static bool isAlignedTo16Byte(ObjectType& d)
+	{
+		return reinterpret_cast<uint64_t>(d.begin()) % 16 == 0;
+	}
+
+	template <int ChannelAmount> span<span<T, MaxSize / ChannelAmount>, ChannelAmount>& split()
+	{
+		static_assert(MaxSize % ChannelAmount == 0, "Can't split with slice length ");
+
+		return *reinterpret_cast<span<span<T, MaxSize / ChannelAmount>, ChannelAmount>*>(this);
+	}
+
+	span<span<float, 4>, MaxSize / 4>& toSimd()
+	{
+		using Type = span<span<float, 4>, MaxSize / 4>;
+
+		static_assert(isSimdable(), "is not SIMDable");
+		jassert(isAlignedTo16Byte());
+
+		return *reinterpret_castspan<Type*>(this);
+	}
+
+	template <class IndexType> const T& operator[](const IndexType& i) const
+	{
+		auto index = i.get(*this);
+		return data[index];
+	}
+
+	template <class IndexType> T& operator[](const IndexType& i)
+	{
+		auto index = i.get(*this);
+		return data[index];
+	}
+
+	const T& operator[](int index) const
+	{
+		return data[index];
+	}
+
+	T& operator[](int index)
+	{
+		return data[index];
+	}
+
+	T* begin() const
+	{
+		return const_cast<T*>(data);
+	}
+
+	T* end() const
+	{
+		return const_cast<T*>(data + MaxSize);
+	}
+
+	void fill(const T& value)
+	{
+		for (auto& v : *this)
+			v = value;
+	}
+
+	constexpr int size()
+	{
+		return MaxSize;
+	}
+
+	static constexpr int alignment()
+	{
+		if (MaxSize * sizeof(T) > 16)
+			return 16;
+		else
+			return MaxSize * sizeof(T);
+	}
+
+	alignas(alignment()) T data[MaxSize];
+};
+
+using float4 = span<float, 4>;
+
+template <class T> struct dyn
+{
+	using Type = dyn<T>;
+	using DataType = T;
+
+	struct wrapped
+	{
+		using ParentType = Type;
+
+		wrapped(int v) : value(v) {};
+		wrapped& operator=(int v) { value = v; return *this; }
+
+		int get(const dyn<T>& obj) const
+		{
+			auto i = value % obj.size;
+			return i;
+		}
+
+		int value = 0;
+	};
+
+	struct zeroed
+	{
+		using ParentType = Type;
+
+		zeroed(int v) : value(v) {};
+		zeroed& operator=(int v) { value = v; return *this; }
+
+		int get(const dyn<T>& obj) const
+		{
+			if (isPositiveAndBelow(value, obj.size))
+				return value;
+
+			return 0;
+		}
+
+		int value = 0;
+	};
+
+	struct clamped
+	{
+		using ParentType = Type;
+
+		clamped() = default;
+		clamped(int v) : value(v) {};
+		clamped& operator=(int v) { value = v; return *this; }
+
+		int get(const dyn<T>& obj) const
+		{
+			return jlimit(0, obj.size, value);
+		}
+
+		int value = 0;
+	};
+
+	struct unsafe
+	{
+		using ParentType = Type;
+
+		unsafe() = default;
+		unsafe(int v) : value(v) {};
+		unsafe& operator=(int v) { value = v; return *this; }
+
+		int get(const dyn<T>& obj) const
+		{
+			return value;
+		}
+
+		int value = 0;
+	};
+
+
+
+	dyn() :
+		data(nullptr),
+		size(0)
+	{}
+
+
+	template<class Other> dyn(Other& o) :
+		data(o.begin()),
+		size(o.end() - o.begin())
+	{}
+
+	template<class Other> dyn(Other& o, size_t size_) :
+		data(o.begin()),
+		size(size_)
+	{}
+
+	dyn(T* data_, size_t size_) :
+		data(data_),
+		size(size_)
+	{}
+
+	template<class Other> dyn(Other& o, size_t size_, size_t offset) :
+		data(o.begin() + offset),
+		size(size_)
+	{
+		auto fullSize = o.end() - o.begin();
+		jassert(offset + size < fullSize);
+	}
+
+
+	dyn<float4> toSimd() const
+	{
+		dyn<float4> rt;
+
+		jassert(size % 4 == 0);
+
+		rt.data = reinterpret_cast<float4*>(begin());
+		rt.size = size / 4;
+
+		return rt;
+	}
+
+	template <int NumChannels> span<dyn<T>, NumChannels> split()
+	{
+		span<dyn<T>, NumChannels> r;
+
+		int newSize = size / NumChannels;
+
+		T* d = data;
+
+		for (auto& e : r)
+		{
+			e = dyn<T>(d, newSize);
+			d += newSize;
+		}
+
+		return r;
+	}
+
+	template <class WrapType> float interpolate(float index)
+	{
+		return DSP::interpolate<WrapType>(*this, index);
+	}
+
+	template <class IndexType> const T& operator[](IndexType& t) const
+	{
+		auto index = t.get(*this);
+		return data[index];
+	}
+
+	template <class IndexType> T& operator[](IndexType& t)
+	{
+		auto index = t.get(*this);
+		return data[index];
+	}
+
+	const T& operator[](int index) const
+	{
+		return data[index];
+	}
+
+	T& operator[](int index)
+	{
+		return data[index];
+	}
+
+	T* begin() const
+	{
+		return const_cast<T*>(data);
+	}
+
+	T* end() const
+	{
+		return const_cast<T*>(data) + size;
+	}
+
+	int unused = 0;
+	int size = 0;
+	T* data;
+
+};
+
+namespace Interleaver
+{
+
+
+static void interleave(float* src, int numFrames, int numChannels)
+{
+	size_t numBytes = sizeof(float) * numChannels * numFrames;
+
+	float* dst = (float*)alloca(numBytes);
+
+	for (int i = 0; i < numFrames; i++)
+	{
+		for (int j = 0; j < numChannels; j++)
+		{
+			auto targetIndex = i * numChannels + j;
+			auto sourceIndex = j * numFrames + i;
+
+			dst[targetIndex] = src[sourceIndex];
+		}
+	}
+
+	memcpy(src, dst, numBytes);
+}
+
+
+static void interleaveRaw(float* src, int numFrames, int numChannels)
+{
+	interleave(src, numFrames, numChannels);
+}
+
+template <class T, int NumChannels> static auto interleave(span<dyn<T>, NumChannels>& t)
+{
+	jassert(isContinousMemory(t));
+
+	int numFrames = t[0].size;
+
+	static_assert(std::is_same<float, T>(), "must be float");
+
+	// [ [ptr, size], [ptr, size] ]
+	// => [ ptr, size ] 
+
+	using FrameType = span<T, NumChannels>;
+
+
+	auto src = reinterpret_cast<float*>(t[0].begin());
+	interleaveRaw(src, numFrames, NumChannels);
+
+	return dyn<FrameType>(reinterpret_cast<FrameType*>(src), numFrames);
+}
+
+/** Interleaves the float data from a dynamic array of frames.
+
+	dyn_span<T>(numChannels) => span<dyn_span<T>, NumChannels>
+*/
+template <class T, int NumChannels> static auto interleave(dyn<span<T, NumChannels>>& t)
+{
+	jassert(isContinousMemory(t));
+
+	int numFrames = t.size;
+
+	span<dyn<T>, NumChannels> d;
+
+	float* src = t.begin()->begin();
+
+
+	interleave(src, NumChannels, numFrames);
+
+	for (auto& r : d)
+	{
+		r = dyn<T>(src, numFrames);
+		src += numFrames;
+	}
+
+	return d;
+}
+
+template <class T, int NumFrames, int NumChannels> static auto& interleave(span<span<T, NumFrames>, NumChannels>& t)
+{
+	static_assert(std::is_same<float, T>(), "must be float");
+	jassert(isContinousMemory(t));
+
+	using ChannelType = span<T, NumChannels>;
+
+	int s1 = sizeof(span<ChannelType, NumFrames>);
+	int s2 = sizeof(span<span<T, NumFrames>, NumChannels>);
+
+
+
+	auto src = reinterpret_cast<float*>(t.begin());
+	interleave(src, NumFrames, NumChannels);
+	return *reinterpret_cast<span<ChannelType, NumFrames>*>(&t);
+}
+
+template <class T> static bool isContinousMemory(const T& t)
+{
+	using ElementType = typename T::DataType;
+
+	auto ptr = t.begin();
+	auto e = t.end();
+
+	auto elementSize = sizeof(ElementType);
+
+	auto size = (e - ptr) * elementSize;
+
+	auto realSize = reinterpret_cast<uint64_t>(e) - reinterpret_cast<uint64_t>(ptr);
+
+	return realSize == size;
+}
+
+
+template <class DataType> static dyn<DataType> slice(const dyn<DataType>& src, int start, int size = -1)
+{
+	if (size == -1)
+		size = src.size - start;
+
+	using ClampType = typename dyn<DataType>::clamped;
+
+	ClampType clampedStart = start;
+	ClampType clampedSize = start + size;
+
+	dyn<DataType> c;
+	c.data = src.begin() + clampedStart.get(src);
+	c.size = clampedSize.get(src) - clampedStart.get(src);
+	return c;
+}
+
+template <class DataType, int MaxSize> static dyn<DataType> slice(const span<DataType, MaxSize>& src, int start, int size = -1)
+{
+	using SpanType = span<DataType, MaxSize>;
+
+	if (size == -1)
+		size = MaxSize - start;
+
+	typename SpanType::clamped clampedStart = { start };
+	typename SpanType::clamped clampedSize = { start + size };
+
+	dyn<DataType> c;
+	c.data = src.begin() + clampedStart.get(src);
+	c.size = clampedSize.get(src) - clampedStart.get(src);
+	return c;
+}
+
+template <int Start, int Size, class DataType, int MaxSize> static span<DataType, Size>& slice(const span<DataType, MaxSize>& src)
+{
+	using SpanType = span<DataType, Size>;
+
+	constexpr int SizeToUse = Size == -1 ? MaxSize - Start : Size;
+
+	typename SpanType::clamped clampedStart = Start;
+	typename SpanType::clamped clampedSize = Start + SizeToUse;
+
+	return *reinterpret_cast<SpanType*>(src.begin() + Start);
+}
+
+template <int C, class T> static auto split(T& t)
+{
+	return t.split<C>();
+}
+};
+
+
+
+
+}
+}
---
+++ hi_snex/snex_jit/snex_jit_NativeDspFunctions.cpp
@@ -0,0 +1,330 @@
+/*  ===========================================================================
+*
+*   This file is part of HISE.
+*   Copyright 2016 Christoph Hart
+*
+*   HISE is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option any later version.
+*
+*   HISE is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with HISE.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Commercial licences for using HISE in an closed source project are
+*   available on request. Please visit the project's website to get more
+*   information about commercial licencing:
+*
+*   http://www.hartinstruments.net/hise/
+*
+*   HISE is based on the JUCE library,
+*   which also must be licenced for commercial applications:
+*
+*   http://www.juce.com
+*
+*   ===========================================================================
+*/
+
+
+namespace snex {
+namespace jit {
+using namespace juce;
+using namespace asmjit;
+
+InbuiltFunctions::InbuiltFunctions(BaseCompiler* compiler) :
+	FunctionClass(NamespacedIdentifier(Identifier()))
+{
+	auto floatType = TypeInfo(Types::ID::Float);
+	auto float2 = new SpanType(floatType, 2);
+	auto dynFloat = new DynType(floatType);
+	ComplexType::Ptr blockType = new SpanType(TypeInfo(dynFloat), 2);
+	ComplexType::Ptr frameType = new DynType(TypeInfo(float2));
+	blockType->setAlias(NamespacedIdentifier("ChannelData"));
+	frameType->setAlias(NamespacedIdentifier("FrameData"));
+
+	compiler->namespaceHandler.registerComplexTypeOrReturnExisting(blockType);
+	compiler->namespaceHandler.registerComplexTypeOrReturnExisting(frameType);
+
+	{
+		auto il = new FunctionData();
+		il->id = getClassName().getChildId("interleave");
+		il->returnType = TypeInfo(Types::ID::Dynamic);
+		il->addArgs("input", TypeInfo(Types::ID::Dynamic));
+
+		il->inliner = new Inliner(il->id, [frameType, blockType, compiler](InlineData* b)
+		{
+			auto d = b->toAsmInlineData();
+
+			auto& cc = d->gen.cc;
+
+			FunctionData iData;
+
+			iData.returnType = TypeInfo(Types::ID::Void);
+			iData.addArgs("ptr", TypeInfo(Types::ID::Pointer, true, true));
+			iData.addArgs("numFrames", TypeInfo(Types::ID::Integer));
+			iData.addArgs("numChannels", TypeInfo(Types::ID::Integer));
+
+			iData.function = Types::Interleaver::interleaveRaw;
+
+			bool isFrame = d->args[0]->getTypeInfo().getComplexType() == frameType;
+			bool isBlock = d->args[0]->getTypeInfo().getComplexType() == blockType;
+
+			jassert(isFrame || isBlock);
+
+			auto dataRegister = compiler->registerPool.getNextFreeRegister(d->target->getScope(), TypeInfo(Types::ID::Pointer, true));
+			auto frameRegister = compiler->registerPool.getNextFreeRegister(d->target->getScope(), TypeInfo(Types::ID::Integer));
+			auto channelRegister = compiler->registerPool.getNextFreeRegister(d->target->getScope(), TypeInfo(Types::ID::Integer));
+
+			auto ptrReg = d->args[0];
+
+			cc.setInlineComment("interleave call");
+
+			dataRegister->createRegister(cc);
+			channelRegister->createRegister(cc);
+			cc.mov(INT_REG_W(channelRegister), 2);
+			frameRegister->createRegister(cc);
+
+			if (isFrame)
+			{
+				X86Mem sizePtr;
+
+				if (ptrReg->isMemoryLocation())
+				sizePtr = ptrReg->getAsMemoryLocation();
+				else
+				sizePtr = x86::ptr(PTR_REG_R(ptrReg));
+
+				sizePtr = sizePtr.cloneAdjustedAndResized(4, 4);
+
+				auto dataPtr = sizePtr.cloneAdjustedAndResized(4, 8);
+				cc.mov(INT_REG_W(frameRegister), sizePtr);
+				cc.mov(PTR_REG_W(dataRegister), dataPtr);
+			}
+			else
+			{
+				X86Mem sizePtr;
+
+				if (ptrReg->isMemoryLocation())
+					sizePtr = ptrReg->getAsMemoryLocation();
+				else
+					sizePtr = x86::ptr(PTR_REG_R(ptrReg));
+
+				sizePtr = sizePtr.cloneAdjustedAndResized(4, 4);
+				cc.mov(INT_REG_W(frameRegister), sizePtr);
+				auto dataPtr = sizePtr.cloneAdjustedAndResized(4, 8);
+				cc.mov(PTR_REG_W(dataRegister), dataPtr);
+
+			}
+
+			AssemblyRegister::List iArgs;
+
+			iArgs.add(dataRegister);
+
+			if (isFrame)
+			{
+				iArgs.add(channelRegister);
+				iArgs.add(frameRegister);
+			}
+			else
+			{
+				iArgs.add(frameRegister);
+				iArgs.add(channelRegister);
+			}
+
+
+
+			d->gen.emitFunctionCall(d->target, iData, nullptr, iArgs);
+
+			if (isFrame)
+			{
+				d->target->createRegister(cc);
+				cc.mov(PTR_REG_W(d->target), PTR_REG_R(dataRegister));
+			}
+			else
+			{
+				auto s = cc.newStack(frameType->getRequiredByteSize(), frameType->getRequiredAlignment());
+
+				cc.mov(s.cloneAdjustedAndResized(0, 4), 0);
+				cc.mov(s.cloneAdjustedAndResized(4, 4), INT_REG_R(frameRegister));
+				cc.mov(s.cloneAdjustedAndResized(8, 8), PTR_REG_R(dataRegister));
+
+				d->target->setCustomMemoryLocation(s, false);
+			}
+
+			dataRegister->flagForReuse();
+			channelRegister->flagForReuse();
+			frameRegister->flagForReuse();
+
+			return Result::ok();
+		}, {});
+
+		il->inliner->returnTypeFunction = [blockType, frameType](InlineData* d)
+		{
+			auto rd = dynamic_cast<ReturnTypeInlineData*>(d);
+
+			rd->object->tryToResolveType();
+
+			auto inType = rd->object->getSubExpr(0)->getTypeInfo();
+
+			if (inType.getComplexType() == blockType)
+			{
+				rd->f.returnType = TypeInfo(frameType, false);
+				rd->f.args.getReference(0).typeInfo = TypeInfo(blockType, false, true);
+				return Result::ok();
+			}
+			else if (inType.getComplexType() == frameType)
+			{
+				rd->f.returnType = TypeInfo(blockType, false);
+				rd->f.args.getReference(0).typeInfo = TypeInfo(frameType, false, true);
+				return Result::ok();
+			}
+
+			return Result::fail("can't deduce correct type for interleave call");
+		};
+
+
+		compiler->namespaceHandler.addSymbol(il->id, il->returnType, NamespaceHandler::Function);
+		addFunction(il);
+	}
+
+	{
+		auto sliceFunction = new FunctionData();
+
+		sliceFunction->id = getClassName().getChildId("slice");
+		sliceFunction->returnType = TypeInfo(Types::ID::Dynamic);
+		sliceFunction->addArgs("obj", TypeInfo(Types::ID::Dynamic, false, true));
+		sliceFunction->addArgs("start", TypeInfo(Types::ID::Integer));
+		sliceFunction->addArgs("size", TypeInfo(Types::ID::Integer));
+
+		sliceFunction->inliner = Inliner::createAsmInliner(sliceFunction->id, [](InlineData* b)
+		{
+			auto d = b->toAsmInlineData();
+
+			auto& cc = d->gen.cc;
+			auto& dst = d->target;
+			auto& src = d->args[0];
+			auto& start = d->args[1];
+			auto& size = d->args[2];
+
+			jassert(dst->getTypeInfo().getTypedIfComplexType<DynType>() != nullptr);
+			jassert(src->getTypeInfo().getTypedIfComplexType<ArrayTypeBase>() != nullptr);
+			jassert(start->getType() == Types::ID::Integer);
+			jassert(size->getType() == Types::ID::Integer);
+
+			auto startReg = cc.newGpd();
+
+			if (start->hasCustomMemoryLocation())
+				cc.mov(startReg, start->getAsMemoryLocation());
+			else if (start->isMemoryLocation())
+				cc.mov(startReg, start->getImmediateIntValue());
+			else
+				cc.mov(startReg, INT_REG_R(start));
+
+			auto sizeReg = cc.newGpd();
+
+			if (size->hasCustomMemoryLocation())
+				cc.mov(sizeReg, size->getAsMemoryLocation());
+			else if (start->isMemoryLocation())
+				cc.mov(sizeReg, size->getImmediateIntValue());
+			else
+				cc.mov(sizeReg, INT_REG_R(size));
+
+			auto maxReg = cc.newGpd();
+
+
+			if (auto st = src->getTypeInfo().getTypedIfComplexType<SpanType>())
+			{
+				cc.mov(maxReg, st->getNumElements());
+			}
+			else
+			{
+				auto tempDataReg = cc.newGpq();
+
+				if (src->isMemoryLocation())
+					cc.lea(tempDataReg, src->getAsMemoryLocation());
+				else
+					cc.mov(tempDataReg, PTR_REG_R(src));
+
+				cc.mov(maxReg, x86::ptr(tempDataReg).cloneAdjustedAndResized(4, 4));
+			}
+
+
+			{
+				auto zero = cc.newGpd();
+				auto valueReg = cc.newGpd();
+				cc.xor_(zero, zero);
+				cc.mov(valueReg, startReg);
+
+				MathFunctions::Intrinsics::range(cc, startReg, valueReg, zero, maxReg);
+
+				cc.sub(maxReg, startReg);
+				cc.add(maxReg, 1);
+				cc.mov(valueReg, sizeReg);
+
+				MathFunctions::Intrinsics::range(cc, sizeReg, valueReg, zero, maxReg);
+			}
+
+			
+
+			auto dataReg = cc.newGpq();
+
+			if (src->isMemoryLocation())
+				cc.lea(dataReg, src->getAsMemoryLocation());
+			else
+				cc.mov(dataReg, PTR_REG_R(src));
+
+			if (auto dt = src->getTypeInfo().getTypedIfComplexType<DynType>())
+				cc.mov(dataReg, x86::ptr(dataReg).cloneAdjustedAndResized(8, 8));
+
+			cc.lea(dataReg, x86::ptr(dataReg, startReg.r64(), 2));// startReg.r64());
+
+			auto stackMem = cc.newStack(16, 0);
+			cc.mov(stackMem.cloneResized(4), 0);
+
+			cc.mov(stackMem.cloneAdjustedAndResized(4, 4), sizeReg);
+			cc.mov(stackMem.cloneAdjustedAndResized(8, 8), dataReg);
+
+			dst->setCustomMemoryLocation(stackMem, false);
+
+			return Result::ok();
+		});
+
+		sliceFunction->inliner->returnTypeFunction = [](InlineData* b)
+		{
+			auto rt = dynamic_cast<ReturnTypeInlineData*>(b);
+
+			auto sourceType = rt->object->getSubExpr(0)->getTypeInfo();
+			TypeInfo elementType;
+			
+
+			if (auto st = sourceType.getTypedIfComplexType<ArrayTypeBase>())
+				elementType = st->getElementType();
+			else
+				return Result::fail("Can't slice type " + sourceType.toString());
+
+			if (rt->templateParameters.isEmpty())
+			{
+				auto sliceType = new DynType(elementType);
+				rt->f.returnType = TypeInfo(sliceType, false, false);
+			}
+			else
+			{
+				jassertfalse;
+			}
+
+			return Result::ok();
+		};
+
+		compiler->namespaceHandler.addSymbol(sliceFunction->id, sliceFunction->returnType, NamespaceHandler::Function);
+		addFunction(sliceFunction);
+	}
+
+	
+}
+
+}
+}
