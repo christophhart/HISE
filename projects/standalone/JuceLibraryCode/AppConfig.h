9cad6be2ed5bba594a0212accd41b9da218e67d5
ustk iMac <g.delsavio@hotmail.fr>
2021-09-01 14:49:54 +0200

    Set HISE_SCRIPT_SERVER_TIMEOUT to a more respectable value

--- projects/standalone/JuceLibraryCode/AppConfig.h
+++ projects/standalone/JuceLibraryCode/AppConfig.h
@@ -123,11 +123,15 @@
 #endif
 
 #ifndef    USE_VDSP_FFT
- //#define USE_VDSP_FFT 0
+ //#define USE_VDSP_FFT 1
 #endif
 
 #ifndef    FRONTEND_IS_PLUGIN
  //#define FRONTEND_IS_PLUGIN 0
+#endif
+
+#ifndef    PROCESS_SOUND_GENERATORS_IN_FX_PLUGIN
+ //#define PROCESS_SOUND_GENERATORS_IN_FX_PLUGIN 1
 #endif
 
 #ifndef    FORCE_INPUT_CHANNELS
@@ -184,6 +188,10 @@
 
 #ifndef    ENABLE_ALL_PEAK_METERS
  #define   ENABLE_ALL_PEAK_METERS 1
+#endif
+
+#ifndef    READ_ONLY_FACTORY_PRESETS
+ //#define READ_ONLY_FACTORY_PRESETS 0
 #endif
 
 #ifndef    ENABLE_CONSOLE_OUTPUT
@@ -285,6 +293,10 @@
  //#define HISE_INCLUDE_SNEX 0
 #endif
 
+#ifndef    SNEX_INCLUDE_MEMORY_ADDRESS_IN_DUMP
+ //#define SNEX_INCLUDE_MEMORY_ADDRESS_IN_DUMP 0
+#endif
+
 //==============================================================================
 // hi_streaming flags:
 
@@ -297,6 +309,14 @@
 
 #ifndef    HISE_NO_GUI_TOOLS
  //#define HISE_NO_GUI_TOOLS 0
+#endif
+
+#ifndef    HISE_USE_NEW_CODE_EDITOR
+ //#define HISE_USE_NEW_CODE_EDITOR 1
+#endif
+
+#ifndef    IS_MARKDOWN_EDITOR
+ //#define IS_MARKDOWN_EDITOR 0
 #endif
 
 //==============================================================================
