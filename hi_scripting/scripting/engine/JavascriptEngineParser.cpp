1a6fcabbca491fb7b75fb11708a2c631ca16534a
chrisboy2000 <christoph-hart@gmx.de>
2022-02-12 11:19:22 +0100

    - move the local definition bug detection on compilation level

--- hi_scripting/scripting/engine/JavascriptEngineParser.cpp
+++ hi_scripting/scripting/engine/JavascriptEngineParser.cpp
@@ -428,6 +428,7 @@ private:
 
 	Identifier fileId;
 
+	DynamicObject* outerInlineFunction = nullptr;
 	DynamicObject* currentInlineFunction = nullptr;
 
 	JavascriptNamespace* currentNamespace = nullptr;
@@ -1446,7 +1447,8 @@ private:
 		// We need to temporarily set the currentInlineFunction to nullptr to avoid
 		// local references inside the nested function body which will fail when
 		// ENABLE_SCRIPTING_BREAKPOINTS is disabled
-		ScopedValueSetter<DynamicObject*> cifMuter(currentInlineFunction, nullptr);
+		ScopedValueSetter<DynamicObject*> svs1(outerInlineFunction, currentInlineFunction);
+		ScopedValueSetter<DynamicObject*> svs2(currentInlineFunction, nullptr);
 
 		ScopedPointer<FunctionObject> fo(new FunctionObject());
 
@@ -1636,9 +1638,20 @@ private:
 			{
 				return parseSuffixes(new LoopStatement::IteratorName(location, parseIdentifier()));
 			}
-			else if (currentInlineFunction != nullptr)
+			else if (auto ob = dynamic_cast<InlineFunction::Object*>(outerInlineFunction))
 			{
-				InlineFunction::Object* ob = dynamic_cast<InlineFunction::Object*>(currentInlineFunction);
+				const int inlineParameterIndex = ob->parameterNames.indexOf(id);
+				const int localParameterIndex = ob->localProperties.indexOf(id);
+
+				if (inlineParameterIndex != -1)
+					location.throwError("Can't reference inline function parameters in nested function body");
+
+				if (localParameterIndex != -1)
+					location.throwError("Can't reference local variables in nested function body");
+			}
+			else if (auto ob = dynamic_cast<InlineFunction::Object*>(currentInlineFunction))
+			{
+				
 
 				const int inlineParameterIndex = ob->parameterNames.indexOf(id);
 				const int localParameterIndex = ob->localProperties.indexOf(id);
