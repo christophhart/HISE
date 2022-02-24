6936625b0b503734fe5dd0ccce7b7896da7fa5cd
chrisboy2000 <christoph-hart@gmx.de>
2022-02-24 14:51:10 +0100

    - fix OpenGL setting glitches

--- hi_components/plugin_components/StandalonePopupComponents.cpp
+++ hi_components/plugin_components/StandalonePopupComponents.cpp
@@ -110,14 +110,14 @@ CustomSettingsWindow::CustomSettingsWindow(MainController* mc_, bool buildMenus)
 	ADD(SampleRate);
 	ADD(GlobalBPM);
 	ADD(ScaleFactor);
+	ADD(UseOpenGL);
 	ADD(StreamingMode);
 	ADD(VoiceAmountMultiplier);
 	ADD(ClearMidiCC);
 	ADD(SampleLocation);
 	ADD(DebugMode);
 	ADD(ScaleFactorList);
-	ADD(UseOpenGL);
-    
+	
 	setColour(ColourIds::textColour, Colours::white);
 
     for(int i = 0; i < (int)Properties::numProperties; i++)
