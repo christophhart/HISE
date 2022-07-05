a89dc0402194abaaa8c975bcfb440656249e8a62
Christoph Hart <>
2022-07-05 17:55:49 +0200

    - fix batch reencode samplemaps crash

--- hi_sampler/sampler/ModulatorSamplerData.cpp
+++ hi_sampler/sampler/ModulatorSamplerData.cpp
@@ -1597,6 +1597,10 @@ BatchReencoder::BatchReencoder(ModulatorSampler* s) :
 	if (GET_HISE_SETTING(s, HiseSettings::Project::SupportFullDynamicsHLAC))
 		getComboBoxComponent("normalise")->setSelectedItemIndex(2, dontSendNotification);
 
+    addComboBox("splitsize", { "1500 MB", "1700 MB", "2000 MB" }, "Split size");
+
+    getComboBoxComponent("splitsize")->setSelectedItemIndex(1, dontSendNotification);
+    
 	addProgressBarComponent(wholeProgress);
 
 	addBasicComponents(true);
