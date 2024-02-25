ca66b1fa2a1b5c11ee8670d4b2f35c2d7e55451c
Christoph Hart <christoph-hart@gmx.de>
2024-02-25 23:52:24 +0100

    - updated windows install script using new multipage creator

--- tools/auto_build/02_BuildInstaller.bat
+++ tools/auto_build/02_BuildInstaller.bat
@@ -1,45 +1,16 @@
-@echo off 
+@echo off
 
 call ConfigWindows.bat
 
-del install_script.iss
 
-set PRODUCT=HISE
-set COMPANY="Hart Instruments"
-set WEBSITE=http://hise.audio/
+cd ..
+cd ..
+%multipage_binary% --export:"%CD%/tools/auto_build/installer/hise_installer.json" --hisepath:"%CD%"
+cd tools/auto_build/installer/Binaries/
 
-set FART_TOOL=fart
+echo %CD%
 
-git describe --abbrev=0 > tmpFile
+call batchCompile.bat
 
-SET /p VERSION_POINT= < tmpFile
+pause
 
-echo Version with point: %VERSION_POINT%
-
-%FART_TOOL% tmpFile . _
-
-SET /p VERSION_UNDERSCORE= < tmpFile
-
-echo Version with underscores: %VERSION_UNDERSCORE%
-
-del tmpFile
-
-SET version=Enter version with underscores for the filename: 
-SET filename=%PRODUCT%_%VERSION_UNDERSCORE%.exe
-
-copy iss_installer_template.iss install_script.iss
-
-%FART_TOOL% install_script.iss $PRODUCT %PRODUCT%
-%FART_TOOL% install_script.iss $COMPANY %COMPANY%
-%FART_TOOL% install_script.iss $WEBSITE %WEBSITE%
-%FART_TOOL% install_script.iss $VERSION_POINT %VERSION_POINT%
-%FART_TOOL% install_script.iss $VERSION_UNDERSCORE %VERSION_UNDERSCORE%
-
-REM =======================================================================================
-REM Building Installer
-
-echo Building Installer...
-
-SET installer_project=install_script.iss
-
-%installer_command% %installer_project%
