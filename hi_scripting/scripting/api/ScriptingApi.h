720b50f8868ef5e3de9482766b56a0e0a4c8849b
David Healey <14021202+davidhealey@users.noreply.github.com>
2025-01-19 20:47:25 +0000

    Added/fixed support of pb and at in sendController and addController (#626)
    * Added/fixed support of pb and at in sendController and addController
    
    * Updated definition variable names to match implementation.
    
    * Fixed a little typo with the channel range
    
    * Fix send/add controller pitch bend range check

--- hi_scripting/scripting/api/ScriptingApi.h
+++ hi_scripting/scripting/api/ScriptingApi.h
@@ -1184,7 +1184,7 @@ public:
 
 
 		/** Sends a controller event to the synth. */
-		void sendController(int controllerNumber, int controllerValue);
+		void sendController(int number, int value);
 
 		/** The same as sendController (for backwards compatibility) */
 		void sendControllerToChildSynths(int controllerNumber, int controllerValue);
