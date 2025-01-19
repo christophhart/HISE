f85f76ae9707f916afdd326a6ffde21b8c9984f6
David Healey <14021202+davidhealey@users.noreply.github.com>
2025-01-19 20:37:01 +0000

    Prevent multiple file browsers being opened at once (#657)

--- hi_scripting/scripting/api/ScriptingApi.cpp
+++ hi_scripting/scripting/api/ScriptingApi.cpp
@@ -7254,6 +7254,13 @@ int64 ScriptingApi::FileSystem::getBytesFreeOnVolume(var folder)
 
 void ScriptingApi::FileSystem::browseInternally(File f, bool forSaving, bool isDirectory, String wildcard, var callback)
 {
+	static bool fileChooserIsOpen = false;
+
+	if (fileChooserIsOpen)
+			return;
+
+	fileChooserIsOpen = true;
+
 	auto p_ = p;
 
 	WeakCallbackHolder wc(p_, this, callback, 1);
@@ -7290,6 +7297,8 @@ void ScriptingApi::FileSystem::browseInternally(File f, bool forSaving, bool isD
 		{
 			wc.call(&a, 1);
 		}
+		
+		fileChooserIsOpen = false;
 	};
 
 	MessageManager::callAsync(cb);
