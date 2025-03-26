4e91ed8e13e5a35a48bb9fd1a6d18654ccee1e9e
dave <dave41264@hotmail.com>
2025-03-26 23:54:26 +0000

    Use pkg-config to detect WebKitGTK version dynamically on Linux

--- JUCE/extras/Projucer/Source/ProjectSaving/jucer_ProjectExporter.cpp
+++ JUCE/extras/Projucer/Source/ProjectSaving/jucer_ProjectExporter.cpp
@@ -512,10 +512,15 @@ static bool isLoadCurlSymbolsLazilyEnabled (Project& project)
             && project.isConfigFlagEnabled ("JUCE_LOAD_CURL_SYMBOLS_LAZILY", false));
 }
 
+bool isPackageAvailable (const juce::String& package)
+{
+    juce::String cmd = "pkg-config --exists " + package;
+    return std::system (cmd.toRawUTF8()) == 0;
+}
+
 StringArray ProjectExporter::getLinuxPackages (PackageDependencyType type) const
 {
     auto packages = linuxPackages;
-    juce::DynamicLibrary library;
 
     // don't add libcurl if curl symbols are loaded at runtime
     if (isCurlEnabled (project) && ! isLoadCurlSymbolsLazilyEnabled (project))
@@ -523,10 +528,10 @@ StringArray ProjectExporter::getLinuxPackages (PackageDependencyType type) const
 
     if (isWebBrowserComponentEnabled (project) && type == PackageDependencyType::compile)
     {
-		if (library.open ("libwebkit2gtk-4.1.so"))
-			packages.add ("webkit2gtk-4.1");
-        else
-			packages.add ("webkit2gtk-4.0");
+      if (isPackageAvailable ("webkit2gtk-4.1"))
+          packages.add ("webkit2gtk-4.1");
+      else if (isPackageAvailable ("webkit2gtk-4.0"))
+          packages.add ("webkit2gtk-4.0");
 			
         packages.add ("gtk+-x11-3.0");
     }
