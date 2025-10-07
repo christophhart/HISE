e3175ca619e316c5f712bb2e30f7d1c3a03a0cfe
Dan Korneff <dankorneff@pop-os.localdomain>
2025-10-07 10:21:05 -0400

    Merge upstream/develop into develop

--- hi_core/hi_components/plugin_components/FrontendBar.cpp
+++ hi_core/hi_components/plugin_components/FrontendBar.cpp
@@ -386,7 +386,6 @@ void DeactiveOverlay::resized()
 		ignoreButton->setVisible(false);
 	}
 
-#if HISE_INCLUDE_UNLOCKER_OVERLAY
 	if (currentState[State::LicenseNotFound] ||
 		currentState[State::LicenseInvalid] ||
 		currentState[State::MachineNumbersNotMatching] ||
@@ -410,7 +409,6 @@ void DeactiveOverlay::resized()
 		resolveLicenseButton->setTopLeftPosition(registerProductButton->getX(),
 			registerProductButton->getY() + 40);
 	}
-#endif
 	
 	if (currentState[State::CriticalCustomErrorMessage])
 	{
