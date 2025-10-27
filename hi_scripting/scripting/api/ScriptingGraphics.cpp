892e4d8180b7c5883f1d1b5e244d6e7d47ef7646
ustk iMac <g.delsavio@hotmail.fr>
2025-10-27 13:38:08 +0100

    Fix label in linear slider preventing mouse detection

--- hi_scripting/scripting/api/ScriptingGraphics.cpp
+++ hi_scripting/scripting/api/ScriptingGraphics.cpp
@@ -4440,6 +4440,8 @@ void ScriptingObjects::ScriptedLookAndFeel::Laf::drawLinearSlider(Graphics &g, i
 	{
 		DynamicObject* obj = new DynamicObject();
 
+		slider.setTextBoxStyle(Slider::NoTextBox, false, -1, -1);
+
 		writeId(obj, &slider);
 		obj->setProperty("enabled", slider.isEnabled());
 		obj->setProperty("text", slider.getName());
