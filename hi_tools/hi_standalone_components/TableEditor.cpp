c0a33841ad85aac1ee612492e20d5ddf7e7cf97f
DHPLUGS <78897383+DHPLUGS@users.noreply.github.com>
2026-06-04 21:23:20 +0100

    improved table drag point handle cursors and hid redundant mid points
    - show a crosshair cursor over draggable table points and an up/down resize
      cursor over mid points (was a generic dragging-hand cursor)
    - don't create a mid point between the two end points when there are only two
      points and syncStartEnd is on, since it is redundant

--- hi_tools/hi_standalone_components/TableEditor.cpp
+++ hi_tools/hi_standalone_components/TableEditor.cpp
@@ -90,7 +90,7 @@ void TableEditor::refreshGraph()
 
 	mid_points.clear();
 
-	if (dragProperties.midPointSize > 0)
+	if (dragProperties.midPointSize > 0 && (drag_points.size() > 2 || !dragProperties.syncStartEnd))
 	{
 		auto width = (float)a.getWidth();
 		float w = (float)dragProperties.midPointSize;
@@ -733,7 +733,7 @@ void TableEditor::mouseMove(const MouseEvent& e)
 {
 	hoveredMidPointIndex = getDraggedMidPointIndex(e);
 
-	setMouseCursor(hoveredMidPointIndex != -1 ? MouseCursor::DraggingHandCursor : MouseCursor::NormalCursor);
+	setMouseCursor(hoveredMidPointIndex != -1 ? MouseCursor::UpDownResizeCursor : MouseCursor::NormalCursor);
 
 	if (e.eventComponent != this)
 	{
@@ -1351,12 +1351,14 @@ void TableEditor::DragPoint::mouseEnter(const MouseEvent& mouseEvent)
 	if (auto te = findParentComponentOfClass<TableEditor>())
 		te->pointAreaBetweenMouse = {};
 
+	setMouseCursor(MouseCursor::CrosshairCursor);
 	over = true;
 	repaint();
 }
 
 void TableEditor::DragPoint::mouseExit(const MouseEvent& mouseEvent)
 {
+	setMouseCursor(MouseCursor::NormalCursor);
 	over = false;
 	repaint();
 }
